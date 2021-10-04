f247e614b743bad1bfe2dfb3259aa3d6d2978e72
Pranav Chaturvedi <61840685+pchat99@users.noreply.github.com>
2021-10-04 22:06:43 +0530

    Update copy_set_bits_in_range.cpp

--- CompanywiseSolution/Amazon/Bit Manipulation/copy_set_bits_in_range.cpp
+++ CompanywiseSolution/Amazon/Bit Manipulation/copy_set_bits_in_range.cpp
@@ -1,3 +1,18 @@
+/* Problem-Statement - Given two numbers x and y, and a range [l, r] where 1 <= l, r <= 32. The task is consider set bits of y in range [l, r] and set these bits in x also.
+
+Examples -
+Input  : x = 10, y = 13, l = 2, r = 3
+Output : x = 14
+Binary representation of 10 is 1010 and 
+that of y is 1101. There is one set bit
+in y at 3'rd position (in given range). 
+After we copy this bit to x, x becomes 1110
+which is binary representation of 14.
+
+Input  : x = 8, y = 7, l = 1, r = 2
+Output : x = 11
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
