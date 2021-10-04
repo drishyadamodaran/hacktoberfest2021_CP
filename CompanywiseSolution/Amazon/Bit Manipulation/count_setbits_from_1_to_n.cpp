db5774f3051532c53ca067bc37371bc8c4667c20
Pranav Chaturvedi <61840685+pchat99@users.noreply.github.com>
2021-10-04 22:08:10 +0530

    Update count_setbits_from_1_to_n.cpp

--- CompanywiseSolution/Amazon/Bit Manipulation/count_setbits_from_1_to_n.cpp
+++ CompanywiseSolution/Amazon/Bit Manipulation/count_setbits_from_1_to_n.cpp
@@ -1,3 +1,23 @@
+/* Problem Statement - You are given a number N. Find the total count of set bits for all numbers from 1 to N(both inclusive).
+
+Examples - Input: N = 4
+Output: 5
+Explanation:
+For numbers from 1 to 4.
+For 1: 0 0 1 = 1 set bits
+For 2: 0 1 0 = 1 set bits
+For 3: 0 1 1 = 2 set bits
+For 4: 1 0 0 = 1 set bits
+Therefore, the total set bits is 5.
+
+
+Input: N = 17
+Output: 35
+Explanation: From numbers 1 to 17(both inclusive), 
+the total number of set bits is 35.
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std; 
 
