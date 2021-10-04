c57adb0cc189d1c3f1886d61764c0591b8e35b14
Pranav Chaturvedi <61840685+pchat99@users.noreply.github.com>
2021-10-04 21:59:35 +0530

    Update 3unique_numbers.cpp

--- CompanywiseSolution/Amazon/Bit Manipulation/3unique_numbers.cpp
+++ CompanywiseSolution/Amazon/Bit Manipulation/3unique_numbers.cpp
@@ -1,3 +1,18 @@
+/* Given an array of integers, every element appears thrice except for one which occurs once.
+
+Find that element which does not appear thrice.
+
+
+Example Input 1:  A = [1, 2, 4, 3, 3, 2, 2, 3, 1, 1]
+Example Output 1: 4
+Explanation:
+    4 occur exactly once
+Example Input 2: A = [0, 0, 0, 1]
+Example Output 2: 1
+*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std; 
 
