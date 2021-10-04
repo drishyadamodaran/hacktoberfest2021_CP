5bbf7c83d1dc5c0d73326c3f9fbb250aaae64b0f
Pranav Chaturvedi <61840685+pchat99@users.noreply.github.com>
2021-10-04 22:03:02 +0530

    Update Count set bits in a integer.cpp

--- CompanywiseSolution/Amazon/Bit Manipulation/Count set bits in a integer.cpp
+++ CompanywiseSolution/Amazon/Bit Manipulation/Count set bits in a integer.cpp
@@ -1,3 +1,19 @@
+/* Problem-Statement - Given a positive integer N, print count of set bits in it. 
+
+Examples - Input: N = 6
+Output: 2
+Explanation:
+Binary representation is '110' 
+So the count of the set bit is 2.
+
+Input: 8
+Output: 1
+Explanation:
+Binary representation is '1000' 
+So the count of the set bit is 1.
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std; 
 
