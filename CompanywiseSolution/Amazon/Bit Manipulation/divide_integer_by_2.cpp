8297ca3135b3b142ab9a54f53c3c2c1a449c890a
Pranav Chaturvedi <61840685+pchat99@users.noreply.github.com>
2021-10-04 22:09:08 +0530

    Update divide_integer_by_2.cpp

--- CompanywiseSolution/Amazon/Bit Manipulation/divide_integer_by_2.cpp
+++ CompanywiseSolution/Amazon/Bit Manipulation/divide_integer_by_2.cpp
@@ -1,3 +1,12 @@
+/* Problem Statement - Given a two integers say a and b. Find the quotient after dividing a by b without using multiplication, division and mod operator.
+
+Examples - Input : a = 10, b = 3
+Output : 3
+
+Input : a = 43, b = -8
+Output :  -5 
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
