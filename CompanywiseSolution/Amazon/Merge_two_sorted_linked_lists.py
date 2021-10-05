de97bbafd80722e369bc931406e2d4d858f258ba
Nikita Dhona <72863753+nikitadhona@users.noreply.github.com>
2021-10-05 16:50:32 +0530

    Create Merge_two_sorted_linked_lists.py

---
+++ CompanywiseSolution/Amazon/Merge_two_sorted_linked_lists.py
@@ -0,0 +1,38 @@
+##Given two sorted linked lists, merge them so that the resulting linked list is also sorted.
+
+def merge_sorted(head1, head2):
+  # if both lists are empty then merged list is also empty
+  # if one of the lists is empty then other is the merged list
+  if head1 == None:
+    return head2
+  elif head2 == None:
+    return head1
+
+  mergedHead = None;
+  if head1.data <= head2.data:
+    mergedHead = head1
+    head1 = head1.next
+  else:
+    mergedHead = head2
+    head2 = head2.next
+
+  mergedTail = mergedHead
+  
+  while head1 != None and head2 != None:
+    temp = None
+    if head1.data <= head2.data:
+      temp = head1
+      head1 = head1.next
+    else:
+      temp = head2
+      head2 = head2.next
+
+    mergedTail.next = temp
+    mergedTail = temp
+
+  if head1 != None:
+    mergedTail.next = head1
+  elif head2 != None:
+    mergedTail.next = head2
+
+  return mergedHead
